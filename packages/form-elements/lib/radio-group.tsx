c43fdee64846575a2f7a364023894118af6b4bcd
Daren Malfait <github@daren.be>
2022-03-21 16:45:05 +0100

    feat(form-elements): add radio-group component

---
+++ packages/form-elements/lib/radio-group.tsx
@@ -0,0 +1,98 @@
+import { cx } from '@daren/utils'
+import { RadioGroup as HeadlessRadioGroup } from '@headlessui/react'
+import { CheckCircleIcon } from '@heroicons/react/solid'
+import * as React from 'react'
+
+interface OptionProps {
+  value: any
+  label: string
+  description?: string
+}
+
+function Option({
+  value,
+  label,
+  description,
+  className,
+  ...props
+}: OptionProps & JSX.IntrinsicElements['div']) {
+  return (
+    <HeadlessRadioGroup.Option
+      value={value}
+      {...props}
+      className={({ active, checked }) =>
+        cx(
+          className,
+          'relative rounded-lg shadow-md px-5 py-4 cursor-pointer flex focus:outline-none group',
+          checked ? 'bg-secondary text-primary' : 'bg-inverse text-inverse',
+          {
+            'ring-2 ring-offset-2 ring-inverse ring-inverse ring-opacity-60':
+              active,
+          },
+        )
+      }
+    >
+      {({ checked }) => (
+        <div className="flex justify-between items-center w-full">
+          <div className="flex items-center">
+            <div className="text-sm">
+              <HeadlessRadioGroup.Label
+                as="p"
+                className={`font-medium  ${
+                  checked ? 'text-primary' : 'text-inverse'
+                }`}
+              >
+                {label}
+              </HeadlessRadioGroup.Label>
+              <HeadlessRadioGroup.Description
+                as="span"
+                className={`inline ${
+                  checked ? 'text-slate-500' : 'text-slate-400'
+                }`}
+              >
+                {description}
+              </HeadlessRadioGroup.Description>
+            </div>
+          </div>
+          {checked && (
+            <div className="shrink-0 text-primary">
+              <CheckCircleIcon className="w-6 h-6" />
+            </div>
+          )}
+        </div>
+      )}
+    </HeadlessRadioGroup.Option>
+  )
+}
+
+interface RadioGroupProps {
+  name: string
+  value?: any
+  label?: string
+  onChange: (value: any) => void
+  children?: React.ReactNode
+}
+
+function RadioGroup({
+  name,
+  value,
+  label,
+  onChange,
+  children,
+}: RadioGroupProps) {
+  return (
+    <HeadlessRadioGroup value={value} onChange={onChange}>
+      <input name={name} value={value} type="hidden" />
+      {label && (
+        <HeadlessRadioGroup.Label className="sr-only">
+          {label}
+        </HeadlessRadioGroup.Label>
+      )}
+      <div className="space-y-2">{children}</div>
+    </HeadlessRadioGroup>
+  )
+}
+
+RadioGroup.Option = Option
+
+export { RadioGroup }
---
+++ packages/form-elements/stories/radio-group.stories.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import * as React from 'react'
+
+import { RadioGroup } from '../lib'
+
+export default {
+  title: 'Form Elements',
+  component: RadioGroup,
+}
+
+export function radioGroup() {
+  const [selected, setSelected] = React.useState<string | null>(null)
+
+  const onChange = React.useCallback(val => {
+    setSelected(val)
+  }, [])
+
+  return (
+    <div>
+      <h2>Options</h2>
+      <RadioGroup name="example-group" value={selected} onChange={onChange}>
+        <RadioGroup.Option
+          value="1"
+          label="Option 1"
+          description="example description"
+        />
+        <RadioGroup.Option value="2" label="Option 2" />
+        <RadioGroup.Option value="3" label="Option 3" />
+        <RadioGroup.Option value="4" label="Option 4" />
+      </RadioGroup>
+    </div>
+  )
+}
