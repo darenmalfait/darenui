b7eb1ac4710315aa08d6adee9c19d7a7a70ba247
Daren Malfait <github@daren.be>
2022-09-28 14:13:44 +0200

    feat(button): add confirm button

---
+++ packages/button/lib/delete-confirm-button.tsx
@@ -0,0 +1,71 @@
+import { Input } from '@daren/form-elements'
+import { H4, Paragraph } from '@daren/typography'
+import * as React from 'react'
+
+import { Button } from './button'
+
+function DeleteConfirmButton({
+  children,
+  confirmString,
+  title = 'Are you sure?',
+  message = `If yes, type in the app name ${confirmString} below`,
+  confirmButtonLabel = 'Yes, Delete it',
+  cancelButtonLabel = 'Cancel',
+  onConfirm,
+  ...props
+}: React.ComponentPropsWithRef<typeof Button> & {
+  onConfirm: () => void
+  confirmString: string
+  message?: string
+  title?: string
+  confirmButtonLabel?: string
+  cancelButtonLabel?: string
+}) {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState('')
+
+  function handleConfirm() {
+    setOpen(false)
+    onConfirm()
+  }
+
+  return (
+    <>
+      <Button variant="danger" {...props} onClick={() => setOpen(!open)}>
+        {children}
+      </Button>
+      {open && (
+        <div className="p-4 mt-4 space-y-2 rounded-md border">
+          <H4>{title}</H4>
+          <Paragraph
+            dangerouslySetInnerHTML={{
+              __html: message.replace(
+                confirmString,
+                `<strong>${confirmString}</strong>`,
+              ),
+            }}
+          />
+          <div className="flex space-x-2 w-full">
+            <Input
+              className="w-full"
+              placeholder="App Name"
+              onChange={e => setValue(e.target.value)}
+            />
+            <Button
+              onClick={handleConfirm}
+              variant="danger"
+              disabled={value !== confirmString}
+            >
+              {confirmButtonLabel}
+            </Button>
+            <Button variant="secondary" onClick={() => setOpen(false)}>
+              {cancelButtonLabel}
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export { DeleteConfirmButton }
--- packages/button/lib/index.ts
+++ packages/button/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './button'
+export * from './delete-confirm-button'
 export * from './link'
