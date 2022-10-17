1b5dd0e186b188a247db5a24f42137596c7faeee
Daren Malfait <github@daren.be>
2022-10-17 18:48:50 +0200

    fix(bionify): ignore some html tags

--- docs/app/routes/bionify.tsx
+++ docs/app/routes/bionify.tsx
@@ -22,7 +22,7 @@ const demoList = [
         code: `function ModalDemo() {
   const [bionified, setBionified] = React.useState(true)
   
-  const text = 'Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic Reading aims to encourage a more in-depth reading and understanding of written content.';
+  const text = '<p>Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic Reading aims to encourage a more in-depth reading and understanding of written content.</p><h2>title in the meantime</h2><p>Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic Reading aims to encourage a more in-depth reading and understanding of written content.</p>';
 
   const html = bionified ? bionify(text) : text;
 
@@ -32,7 +32,7 @@ const demoList = [
         <span>Bionified</span>
         <Toggle onChange={(checked) => setBionified(checked)} value={bionified} />
       </div>
-      <p dangerouslySetInnerHTML={{__html: html }} />
+      <div className="prose" dangerouslySetInnerHTML={{__html: html }} />
     </div>
   )
 }`,
