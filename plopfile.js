e45cfcbe4116ec1e4d815748d34e5e2a3b958d47
Daren Malfait <me@daren.be>
2023-01-13 16:22:33 +0100

    fix(plop): update template script

---
+++ plopfile.js
@@ -0,0 +1,47 @@
+const capitalize = str => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+const camelCase = str => {
+  return str.replace(/[-_](\w)/g, (_, c) => c.toUpperCase())
+}
+
+module.exports = function main(plop) {
+  plop.setHelper('capitalize', text => {
+    return capitalize(camelCase(text))
+  })
+
+  plop.setGenerator('component', {
+    description: 'Generates a component package',
+    prompts: [
+      {
+        type: 'input',
+        name: 'componentName',
+        message: 'Enter component name:',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'The description of this component:',
+      },
+    ],
+    actions(answers) {
+      const actions = []
+
+      if (!answers) return actions
+
+      const {componentName, description} = answers
+
+      actions.push({
+        type: 'addMany',
+        templateFiles: 'plop/component/**',
+        destination: `./packages/{{dashCase componentName}}`,
+        base: 'plop/component',
+        data: {description, componentName},
+        abortOnFail: true,
+      })
+
+      return actions
+    },
+  })
+}
