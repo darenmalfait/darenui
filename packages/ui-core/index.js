2c7cf5f7abf072b32e3f4a4204501d089a7dbedf
Daren Malfait <me@daren.be>
2023-01-29 17:02:26 +0100

    fix(typography): add not-prose check

--- packages/ui-core/index.js
+++ packages/ui-core/index.js
@@ -76,39 +76,42 @@ module.exports = plugin(
           {},
       },
       // keep these styles in sync with the ones in packages/ui-core/src/components/typography/
-      '.prose h1': {
+      // not .not-prose prose
+
+      '.prose h1:not(:where([class~="not-prose"] *))': {
         '@apply scroll-m-20 font-title text-4xl font-extrabold lg:text-5xl !leading-normal':
           {},
       },
-      '.prose h2': {
+      '.prose h2:not(:where([class~="not-prose"] *))': {
         '@apply mt-10 first:mt-0 scroll-m-20 font-title text-3xl !leading-normal':
           {},
       },
-      '.prose h3': {
+      '.prose h3:not(:where([class~="not-prose"] *))': {
         '@apply mt-8 first:mt-0 scroll-m-20 text-2xl font-semibold tracking-tight':
           {},
       },
-      '.prose h4': {
+      '.prose h4:not(:where([class~="not-prose"] *))': {
         '@apply mt-8 first:mt-0 scroll-m-20 text-xl font-semibold tracking-tight':
           {},
       },
-      '.prose p': {
+      '.prose p:not(:where([class~="not-prose"] *))': {
         '@apply leading-7 mt-6 first:mt-0': {},
       },
-      '.prose blockquote': {
+      '.prose blockquote:not(:where([class~="not-prose"] *))': {
         '@apply mt-6 first:mt-0 border-gray-300 italic text-gray-700 dark:border-gray-700 dark:text-gray-400 ltr:border-l-2 ltr:pl-6 rtl:border-r-2 rtl:pr-6':
           {},
       },
-      '.prose ul': {
+      '.prose ul:not(:where([class~="not-prose"] *))': {
         '@apply list-disc first:mt-0 ltr:ml-6 rtl:mr-6': {},
       },
-      '.prose ol': {
+      '.prose ol:not(:where([class~="not-prose"] *))': {
         '@apply list-decimal first:mt-0 ltr:ml-6 rtl:mr-6': {},
       },
-      '.prose ol li, .prose ul li': {
-        '@apply my-2': {},
-      },
-      '.prose > div': {
+      '.prose ol li:not(:where([class~="not-prose"] *)), .prose ul li:not(:where([class~="not-prose"] *))':
+        {
+          '@apply my-2': {},
+        },
+      '.prose > div:not(:where([class~="not-prose"] *))': {
         '@apply mt-6 first:mt-0': {},
       },
     })
