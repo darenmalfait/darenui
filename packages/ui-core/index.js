6566dcdfa4eebc9da9ebbd5f5c8e9b6f68f5c0b8
Daren Malfait <me@daren.be>
2023-03-10 19:48:09 +0100

    fix(core): adjust darkest gray

--- packages/ui-core/index.js
+++ packages/ui-core/index.js
@@ -14,7 +14,7 @@ module.exports = plugin(
         '--color-gray-600': '#585858',
         '--color-gray-700': '#404140',
         '--color-gray-800': '#282928',
-        '--color-gray-900': '#101110',
+        '--color-gray-900': '#161616',
       },
       '.focus-ring': {
         '@apply focus:outline-none focus-within:outline-none transition duration-300 disabled:ring-0 hover:ring-2 focus:ring-2 focus-within:ring-2 group-hover:ring-2 group-focus:ring-2 hover:ring-accent focus:ring-accent focus-within:ring-accent group-hover:ring-accent group-focus:ring-accent ring-accent ring-offset-4 dark:ring-offset-gray-900 ring-offset-white dark:hover:ring-accent-100 dark:focus:ring-accent-100 dark:focus-within:ring-accent-100 dark:group-hover:ring-accent-100 dark:group-focus:ring-accent-100 dark:ring-accent-100':
